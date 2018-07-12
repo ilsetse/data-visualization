8b93cac485ce1c664c7542e2aeaed688b6db1d6f
ilsetse <ilsetse@gmail.com>
2018-07-12 19:44:31 +0300

    made openlayers map demo

--- js/openlayers-demo.js
+++ js/openlayers-demo.js
@@ -1,1 +1,12 @@
-
+var map = new ol.Map({
+	target: 'openlayers-demo',
+	layers: [
+		new ol.layer.Tile({
+			source: new ol.source.OSM()
+		})
+	],
+	view: new ol.View({
+		center: ol.proj.fromLonLat([-96, 37.8]),
+		zoom: 4
+	})
+});
